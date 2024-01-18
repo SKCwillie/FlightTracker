0ed5efff05d6a79f0d29866dd1702b19fc001af7
Robert <rwilliams651@gmail.com>
2024-01-17 22:10:06 -0600

    fixing flow

--- track.py
+++ track.py
@@ -1,14 +1,13 @@
 from models import Flight, fr
 import time
 
-global last_flight
-last_flight = None
+
 # Downtown Kansas City Airport
 LAT = 39.12587
 LON = -94.58754
 DIST = 50000
 PAUSE = 30
-
+last_flight = None
 
 
 def print_flight(last_flight_id):
@@ -16,19 +15,17 @@ def print_flight(last_flight_id):
     response = fr.get_flights(bounds=bounds)
 
     if len(response) == 0:
-        print('empty list')
         pass
     else:
         flight_id = response[0].id
         if flight_id == last_flight_id:
-            print('recursive call')
+            time.sleep(PAUSE)
             return print_flight(last_flight_id)
         else:
-            last_flight = flight_id
             print(Flight(response[0]))
+            return flight_id
 
 
 while True:
-    print('new function call')
-    print_flight(last_flight)
+    last_flight = print_flight(last_flight)
     time.sleep(PAUSE)
